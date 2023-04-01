fa664fca2ea3b8af52b517a56eeb008c93b335b2
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2023-04-01 11:58:44 +0300

    new function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def function(str):
+    """My docstring"""
     return str.uppper()
+
+def function2(str):
+    """Docstring"""
+    list = str.split(" ")
+    output = []
+    for word in list:
+        output.append(word.title())
+    return " ".join(output)
